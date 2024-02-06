f2ebee6242333ae8282a7babe00035115571c3b8
WangsSmall <15093846650@163.com>
2024-02-06 16:22:16 +0800

    feat:实现用户接口调用关系基础功能。实现调用接口信息更新

---
+++ src/main/java/com/yupi/project/service/UserInterfaceInfoService.java
@@ -0,0 +1,28 @@
+package com.yupi.project.service;
+
+import com.yupi.project.model.entity.UserInterfaceInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 75654
+* @description 针对表【user_interface_info(用户调用接口关系)】的数据库操作Service
+* @createDate 2024-02-06 14:48:38
+*/
+public interface UserInterfaceInfoService extends IService<UserInterfaceInfo> {
+
+    /**
+     * 校验
+     *
+     * @param userInterfaceInfo
+     * @param add 是否为创建校验
+     */
+    void validUserInterfaceInfo(UserInterfaceInfo userInterfaceInfo, boolean add);
+
+    /**
+     * 接口调用统计
+     * @param interfaceInfoId 接口id
+     * @param userId 用户 id
+     * @return true、false
+     */
+    boolean invokeCount(long interfaceInfoId, long userId);
+}
